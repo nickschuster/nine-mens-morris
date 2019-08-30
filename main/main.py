b8d70baeda30948254c733f3817452cbd22a6ac7
Nikolas Schuster <ngpnickschuster@gmail.com>
2019-08-30 16:10:39 -0700

    Created game assest.

--- main/main.py
+++ main/main.py
@@ -3,7 +3,14 @@ import pygame
 
 pygame.init();
 
-display = pygame.display.set_mode((600,600));
+gameWidth = 750;
+gameHeight = 750;
+
+black = (0,0,0);
+white = (255,255,255);
+red = (255, 0, 0);
+
+display = pygame.display.set_mode((gameHeight, gameWidth));
 
 pygame.display.set_caption('Nine Men\'s Morris');
 
@@ -12,11 +19,9 @@ clock = pygame.time.Clock();
 crashed = False;
 
 while not crashed:
-	for event in pygame.event.get();
+	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			crashed = True;
-
-		print(event);
 
 		pygame.display.update();
 
