dcc0125db31e8405ff98a147351b5cf9f343547b
Nikolas Schuster <ngpnickschuster@gmail.com>
2019-09-01 14:24:40 -0700

    Loading and placement of game assets.

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 #draws all game assets and handels all pygame logic
 import pygame
+from board import Board
 
 pygame.init();
 
@@ -13,6 +14,27 @@ red = (255, 0, 0);
 display = pygame.display.set_mode((gameHeight, gameWidth));
 
 pygame.display.set_caption('Nine Men\'s Morris');
+
+boardImg = pygame.image.load("../assets/ninemensboard.png");
+whitePiece = pygame.image.load("../assets/whitepiece.png");
+blackPiece = pygame.image.load("../assets/blackpiece.png");
+
+display.blit(boardImg, (0,0));
+
+gameBoard = Board();
+
+i = 0;
+for pos in gameBoard.XYPoints:
+	if(i < 9):
+		display.blit(whitePiece, (pos[0] - 25,pos[1] - 25));
+	elif(i < 18):
+		display.blit(blackPiece, (pos[0] - 25,pos[1] - 25));
+	else:
+		pygame.draw.circle(display, red, pos, 20);
+
+	i = i + 1
+
+pygame.display.flip();
 
 clock = pygame.time.Clock();
 
