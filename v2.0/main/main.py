931c6a2deeaaf0d2d823f5a61a8fe2a817acc716
Nikolas Schuster <ngpnickschuster@gmail.com>
2019-10-01 06:11:26 -0700

    Working on menu.

--- v2.0/main/main.py
+++ v2.0/main/main.py
@@ -1,20 +1,52 @@
 import pygame
 from game import Game
 
+# Dimensions
 WINDOW_WIDTH = 1050
 WINDOW_HEIGHT = 750
 
 # For default look
 BOARD_IMG = pygame.image.load("../assets/ninemensboard.png")
 
+# Colours
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (190,45,45)
+LIGHT_RED = (255,0,0)
+
 # Sets up the window
 def setup():
     pygame.init()
     pygame.display.set_caption('Nine Men\'s Morris')
     display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    display.blit(BOARD_IMG, (0,0))
-    pygame.display.flip()
-    return display
+    clock = pygame.time.Clock()
+    return display, clock
+
+# Handles menu events
+def menu(display, clock):
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.fill(WHITE)
+
+        font = pygame.font.Font('freesansbold.ttf', 32) 
+        text = font.render('GeeksForGeeks', True, BLACK, BLACK) 
+        textRect = text.get_rect()  
+        textRect.center = (500, 500) 
+
+        mouse = pygame.mouse.get_pos()
+        print(mouse)
+
+        #pygame.draw.rect()
+
+        
+        pygame.display.update()
+        clock.tick(30)
+
 
 # Starts a new game of nine mens morris
 def newGame(display):
@@ -22,6 +54,8 @@ def newGame(display):
     game.start()
 
 if __name__ == "__main__":
-    display = setup()
-    newGame(display)
+    display, clock = setup()
+    menu(display, clock)
+
+    #newGame(display)
     
