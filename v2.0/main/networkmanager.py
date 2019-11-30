e95c0c2609064286fb27c1bb461c3f21a37d9ef1
Nikolas Schuster <ngpnickschuster@gmail.com>
2019-11-30 09:19:11 -0800

    Added connection to server.

--- v2.0/main/networkmanager.py
+++ v2.0/main/networkmanager.py
@@ -3,19 +3,57 @@ import threading
 import pygame
 from player import Player
 
-THIS_PLAYER = None
+PLAYER_TYPE = None
+HOST = None
+SERVER = '192.168.0.64'
+PORT = 12345
 
-# Connects to server. Connects to opponent. Decides who goes first.
+# Connects to server. Connects to opponent. Decides who goes first. 
+# Affects the two globals PLAYER_TYPE and HOST
 #
 # Returns true or false depending on if the connection could be established.
 def setUpConnection():
+	global PLAYER_TYPE
+	global HOST
+	# Determine what
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((HOST, PORT))
+	# Player type
+	PLAYER_TYPE = sock.recv(1).decode('utf-8')
+	if(PLAYER_TYPE == "C"):
+		# Host IP address
+		HOST = sock.recv(4)
+		# Close the server connection
+		sock.close()
+
+		# Connect to the host
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+	else:
+		# Close the server connection
+		sock.close()
+
+		# Wait for a client connection
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+		sock.bind((HOST, PORT))
+		sock.listen(1)
+		print("Connection: ", sock.getsockname())
+		sc, sockname = sock.accept()
+		print("Client: ", sockname)
 	return False
 
 def getPlayerOne():
-	return OnlinePlayer()
+	if PLAYER_TYPE == "H":
+		return Player()
+	else:
+		return OnlinePlayer()
 
 def getPlayerTwo():
-	return OnlinePlayer()
+	if PLAYER_TYPE == "H":
+		return OnlinePlayer()
+	else:
+		return Player()
 
 class OnlinePlayer(Player):
 	# Constructor
